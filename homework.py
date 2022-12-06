d71e1f3e0fd54cd62b941f4c04b5232aabcf9a27
Стас Богданов <bogdanov_stanislav1@icloud.com>
2022-12-07 00:36:21 +0300

    V1.8

--- homework.py
+++ homework.py
@@ -155,5 +155,5 @@ if __name__ == '__main__':
     ]
 
     for workout_type, data in packages:
-        training = read_package(workout_type, data)  
+        training = read_package(workout_type, data)
         main(training)
